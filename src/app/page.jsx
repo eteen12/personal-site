db0f046b893df5eaf128fcf0183c44805e3ecf21
eteen12 <eteensmash@gmail.com>
2024-11-15 13:50:12 -0800

    logo added

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -180,12 +180,12 @@ export default async function Home() {
       <Container className="mt-24 sm:mt-32 md:mt-56">
         <FadeIn className="max-w-3xl">
           <h1 className="font-display text-5xl font-medium tracking-tight text-neutral-950 [text-wrap:balance] sm:text-7xl">
-            Award-winning development studio based in Denmark.
+            Hi, I&apos;m Ethan Breitkreutz – A Web Developer, and Student
           </h1>
           <p className="mt-6 text-xl text-neutral-600">
-            We are a development studio working at the intersection of design
-            and technology. It’s a really busy intersection though — a lot of
-            our staff have been involved in hit and runs.
+            I specialize in creating responsive and fast, user-friendly websites
+            using modern technologies like Next.js and Tailwind CSS. All my code
+            is written from the ground up, without any site builders.
           </p>
         </FadeIn>
       </Container>
--- src/components/RootLayout.jsx
+++ src/components/RootLayout.jsx
@@ -127,11 +127,11 @@ function Navigation() {
   return (
     <nav className="mt-px font-display text-5xl font-medium tracking-tight text-white">
       <NavigationRow>
-        <NavigationItem href="/work">Our Work</NavigationItem>
-        <NavigationItem href="/about">About Us</NavigationItem>
+        <NavigationItem href="/work">My Work</NavigationItem>
+        <NavigationItem href="/process">Contact</NavigationItem>
       </NavigationRow>
       <NavigationRow>
-        <NavigationItem href="/process">Our Process</NavigationItem>
+        <NavigationItem href="/about">About Me</NavigationItem>
         <NavigationItem href="/blog">Blog</NavigationItem>
       </NavigationRow>
     </nav>
@@ -215,7 +215,7 @@ function RootLayoutInner({ children }) {
                 <div className="grid grid-cols-1 gap-y-10 pb-16 pt-10 sm:grid-cols-2 sm:pt-16">
                   <div>
                     <h2 className="font-display text-base font-semibold text-white">
-                      Our offices
+                      Location
                     </h2>
                     <Offices
                       invert
@@ -224,7 +224,7 @@ function RootLayoutInner({ children }) {
                   </div>
                   <div className="sm:border-l sm:border-transparent sm:pl-16">
                     <h2 className="font-display text-base font-semibold text-white">
-                      Follow us
+                      Socials
                     </h2>
                     <SocialMedia className="mt-6" invert />
                   </div>
